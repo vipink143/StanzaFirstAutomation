80005d3578acfa40190c5782e04a4872927fd755
vipink143 <yadavvipinkumar4@gmail.com>
2025-02-21 09:29:05 +0530

    somefile

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  
   e2e: {
     watchForFileChanges:false ,
     pageLoadTimeout:60000,
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports', // Directory where reports will be saved
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: "mmddyyyy_HHMMss", // Adds timestamp to reports
+    },
     setupNodeEvents(on, config) {
 
       require('cypress-mochawesome-reporter/plugin')(on);
