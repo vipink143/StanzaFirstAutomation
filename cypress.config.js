f9e3c8a4fc7b13e737fc807ebeaf4b244f2e7018
vipink143 <yadavvipinkumar4@gmail.com>
2025-02-21 09:24:42 +0530

    report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     watchForFileChanges:false ,
     pageLoadTimeout:60000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+     
     },
   },
   env:{
